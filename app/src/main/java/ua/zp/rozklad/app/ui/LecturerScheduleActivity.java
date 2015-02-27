1d0440b7634cb97426c53cda8f95fa4c60dfa5fb
vojkovladimir <vojkovladimir@gmail.com>
2015-02-28 01:03:01 +0200

    Fix adding lecturer schedule frgament

--- app/src/main/java/ua/zp/rozklad/app/ui/LecturerScheduleActivity.java
+++ app/src/main/java/ua/zp/rozklad/app/ui/LecturerScheduleActivity.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -47,11 +46,15 @@ public class LecturerScheduleActivity extends BaseActivity
 
         cursor.close();
 
-        Fragment fragment = ScheduleOfWeekFragment.newInstance(lecturerId);
-        getFragmentManager()
-                .beginTransaction()
-                .add(R.id.container, fragment)
-                .commit();
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
+
+        if (fragment == null) {
+            fragment = ScheduleOfWeekFragment.newInstance(lecturerId);
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, fragment)
+                    .commit();
+        }
     }
 
     @Override
