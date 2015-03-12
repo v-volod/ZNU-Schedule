91b8c4cea7effb43f3df593a9c862ea80a6e91f1
Maxim <kkxmshu@gmail.com>
2015-03-12 21:07:18 +0200

    Initial branch commit

--- app/src/main/java/ua/zp/rozklad/app/appwidget/ScheduleWidgetProvider.java
+++
@@ -1,9 +0,0 @@
-package ua.zp.rozklad.app.appwidget;
-
-import android.appwidget.AppWidgetProvider;
-
-/**
- * Created by kkxmshu on 11.03.15.
- */
-public class ScheduleWidgetProvider extends AppWidgetProvider {
-}
