2dbeb4c0d9a927ea92e875f49916a91a65f374cf
Maxim <kkxmshu@gmail.com>
2015-03-13 17:50:51 +0200

    Re-initial commit

---
+++ app/src/main/java/ua/pp/rozkladznu/app/appwidget/ScheduleWidgetProvider.java
@@ -0,0 +1,10 @@
+package ua.pp.rozkladznu.app.appwidget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by kkxmshu on 13.03.15.
+ */
+public class ScheduleWidgetProvider extends AppWidgetProvider {
+
+}
